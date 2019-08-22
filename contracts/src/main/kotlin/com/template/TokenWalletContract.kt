70c52714610c51835f4d0025b00ee857d8bebbd9
kimrusser <52910707+kimrusser@users.noreply.github.com>
2019-08-22 16:12:00 +0800

    Updated TokenWalletContract

--- contracts/src/main/kotlin/com/template/TokenWalletContract.kt
+++ contracts/src/main/kotlin/com/template/TokenWalletContract.kt
@@ -1,6 +1,7 @@
 package com.template
 
 import com.r3.corda.lib.tokens.contracts.EvolvableTokenContract
+import com.template.states.TokenWalletState
 import net.corda.core.contracts.*
 import net.corda.core.transactions.LedgerTransaction
 
@@ -21,8 +22,13 @@ class TokenWalletContract: Contract {
 
         when (command.value){
             is Commands.Register -> requireThat {
-
-            }
+                "There are no inputs." using (tx.inputs.isEmpty())
+                "Only one output state should be created" using (tx.outputs.size == 1)
+                val outputRegister = tx.outputsOfType<TokenWalletState>().single()
+                "Must be signed by the Registering node" using (command.signers.toSet() == outputRegister.participants.map { it.owningKey }.toSet())
+                "Password must contain at least 6 characters with at least one Uppercase, at least one number" using (outputRegister.password.length>=6)
+//                "Password must contain at least 6 characters with with at least one Uppercase, at least one number" using (outputRegister.password.)
+        }
 
             is Commands.Preorder -> requireThat{
 
