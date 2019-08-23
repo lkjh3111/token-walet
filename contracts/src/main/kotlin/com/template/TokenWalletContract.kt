45aae5457e2821fc1f6a2d97065bb2317887594b
kimrusser <52910707+kimrusser@users.noreply.github.com>
2019-08-23 14:29:02 +0800

    updated contracts

--- contracts/src/main/kotlin/com/template/TokenWalletContract.kt
+++ contracts/src/main/kotlin/com/template/TokenWalletContract.kt
@@ -24,20 +24,26 @@ class TokenWalletContract: Contract {
 
         when (command.value){
             is Commands.Register -> requireThat {
-                "There are no inputs." using (tx.inputs.isEmpty())
+                "Registering should not consume registration" using (tx.inputs.isEmpty())
                 "Only one output state should be created" using (tx.outputs.size == 1)
                 val outputRegister = tx.outputsOfType<TokenWalletState>().single()
+                val user = tx.outputStates.single() as TokenWalletState
                 "Must be signed by the Registering node" using (command.signers.toSet() == outputRegister.participants.map { it.owningKey }.toSet())
                 "Password must contain at least 6 characters with at least one Uppercase, at least one number" using (outputRegister.password.length>=6)
+                "Amount must be greater than 0" using (user.wallet.first().quantity>0)
+                "Amount must be greater than 0" using (user.wallet.last().quantity>0)
 //                "Password must contain at least 6 characters with with at least one Uppercase, at least one number" using (outputRegister.password.)
         }
 
             is Commands.Preorder -> requireThat{
 
+
             }
 
             is Commands.Transfer -> requireThat {
-//                "A transaction should only consume one input state." using (tx.inputs.size == 1)
+
+//
+                "A transaction should only consume one input state." using (tx.inputs.size == 1)
 //                "An  transaction should only create one output state." using (tx.outputs.size == 1)
 //                val input = tx.inputStates
 //                val output = tx.outputStates.single() as TokenWalletState
