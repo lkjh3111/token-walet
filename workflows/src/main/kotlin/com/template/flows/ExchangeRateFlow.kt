1b970e49f33c205da057a693fc9b3554a652be35
lkjh3111 <john@quantumcrowd.com>
2019-08-27 14:31:17 +0800

    fixed exchange rate flow

---
+++ workflows/src/main/kotlin/com/template/flows/ExchangeRateFlow.kt
@@ -0,0 +1,102 @@
+package com.template.flows
+
+import co.paralleluniverse.fibers.Suspendable
+import com.r3.corda.lib.tokens.contracts.types.TokenType
+import com.r3.corda.lib.tokens.contracts.utilities.of
+import com.r3.corda.lib.tokens.money.FiatCurrency
+import com.r3.corda.lib.tokens.workflows.utilities.getPreferredNotary
+import com.template.TokenWalletContract
+import com.template.states.PreorderState
+import com.template.states.TokenWalletState
+import net.corda.core.contracts.Amount
+import net.corda.core.contracts.Command
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.flows.StartableByRPC
+import net.corda.core.node.services.queryBy
+import net.corda.core.node.services.vault.QueryCriteria
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+
+@StartableByRPC
+class ExchangeRateFlow(
+        private val convert_from: String,
+        private val to: String,
+        private val ownerId: String) : Test()
+{
+    @Suspendable
+    override fun call(): SignedTransaction
+    {
+        return recordTransactionsWithoutOtherParty(verifyAndSign(registration()))
+    }
+    private fun inputStateAndRef(id: UniqueIdentifier): StateAndRef<TokenWalletState> {
+        val criteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(id))
+        return serviceHub.vaultService.queryBy<TokenWalletState>(criteria = criteria).states.single()
+    }
+    private fun outState(): TokenWalletState
+    {
+        val input = inputStateAndRef(stringToLinearId(ownerId)).state.data
+        return TokenWalletState(
+                input.username,
+                input.password,
+                newWallet(),
+                listOf(ourIdentity),
+                input.linearId
+        )
+    }
+
+    private fun newWallet(): MutableList<Amount<TokenType>>
+    {
+        print (getExchangeRate())
+        print (" ")
+        val input = inputStateAndRef (stringToLinearId(ownerId)).state.data
+        //val token = FiatCurrency.getInstance(convert_from)
+        val wallet = input.wallet.toMutableList()
+        val wal = input.wallet.find{ l -> l.token.tokenIdentifier == convert_from}
+        val wal2 = input.wallet.find{ l -> l.token.tokenIdentifier == to}
+        //Amount.fromDecimal()
+
+        print (wal!!.quantity)
+        print (" ")
+        val newValue = when (convert_from) {
+            "PHP" -> ((wal.quantity.toDouble())*0.01) / getExchangeRate().toLong()
+            "USD" -> ((wal.quantity.toDouble())*0.01) * getExchangeRate().toLong()
+            else -> null
+        }
+        print (newValue)
+        print (" ")
+
+        val new = when (to) {
+            "PHP" -> wal2?.plus(newValue!! of TokenType("PHP", 2))
+            "USD" -> wal2?.plus(newValue!! of TokenType("USD", 2))
+            else -> null
+        }
+
+        print(new)
+        print (" ")
+        val sub = wal.minus(wal)
+        val index = input.wallet.indexOf(wal)
+        wallet.removeAt(index)
+        wallet.add(index,new!!)
+        val index2 = input.wallet.indexOf(wal2)
+        wallet.removeAt(index2)
+        wallet.add(index2,sub)
+        return wallet
+    }
+
+    private fun registration() = TransactionBuilder(notary = getPreferredNotary(serviceHub)).apply {
+        val cmd = Command(TokenWalletContract.Commands.Exchange(), listOf(ourIdentity.owningKey))
+        addInputState(inputStateAndRef (stringToLinearId(ownerId)))
+        addOutputState(outState())
+        addCommand(cmd)
+    }
+    private fun getExchangeRate(): Double {
+        val url = "https://api.exchangeratesapi.io/latest?base=USD&symbols=PHP,USD"
+        val response = khttp.get(url)
+        try {
+            return response.jsonObject.optJSONObject("rates").getDouble("PHP")
+        } catch (e: Exception) {
+            throw IllegalArgumentException()
+        }
+    }
+}
