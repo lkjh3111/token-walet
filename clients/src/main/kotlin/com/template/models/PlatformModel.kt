fc706009a989f84ace2d0e78915b914d8d5c56a2
Jhan Domingo <jhan.domingo@mcveanpacific.com>
2019-08-28 10:29:23 +0800

    revised PlatformModel

--- clients/src/main/kotlin/com/template/models/PlatformModel.kt
+++ clients/src/main/kotlin/com/template/models/PlatformModel.kt
@@ -1,8 +1,23 @@
 package com.template.webserver.models
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.Party
 
+data class PlatformModel (
+        val amount: Long,
+        val currency: String,
+        val ownerId: String,
+        val participants: List<Party>,
+        val linearId: UniqueIdentifier
 
+)
+
+data class ExhangeRate (
+        private val convert_from: String,
+        private val to: String,
+        private val ownerId: String
+)
 
 data class AcceptPreOrder @JsonCreator constructor(
         val approve_request: Boolean,
